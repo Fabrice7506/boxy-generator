214a864fa068351c579e07001a01a33f222e47a7
fabrice <sieguy7506@gmail.com>
2024-11-21 17:03:43 +0000

    code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='p-4 border-t-2 border-gray-100'>
-        <p className='text-center uppercase'>Boxy - All right reserved © Fabcrice Sie</p>
+        <p className='text-center uppercase'>Boxy - All right reserved © Fabrice Sie</p>
     </footer>
   )
 }
